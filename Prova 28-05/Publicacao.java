85ce7d70741fc7af9a1241effd076bb6d11b9b6f
David Alexandre <143817175+DavidASN@users.noreply.github.com>
2024-05-29 08:33:04 -0300

    Update Publicacao.java

--- Prova 28-05/Publicacao.java
+++ Prova 28-05/Publicacao.java
@@ -9,18 +9,32 @@ public class Publicacao {
         this.anoPublicacao = anoPublicacao;
     }
  
-   
     //Titulo
-    String getTitulo() { return this.titulo; }
-    void setTitulo(String titulo) { this.titulo = titulo; }
-
+    String getTitulo() { 
+    return this.titulo; 
+   }
+    
+    void setTitulo(String titulo) { 
+    	this.titulo = titulo;
+    }
+    
     //Autor
-    String getAutor() { return this.autor; }
-    void setAutor(String autor) { this.autor = autor; }
-
-    //Ano
-    int getAnoPub() { return this.anoPublicacao; }
-    void setAnoPub(int anoPublicacao) { this.anoPublicacao = anoPublicacao; }
+    String getAutor() {
+    	return this.autor; 
+    }
+    
+    void setAutor(String autor) {
+    	this.autor = autor; 
+    }
+    
+    //Ano de Publicação
+    int getAnoPub() { 
+    	return this.anoPublicacao; 
+    }
+    
+    void setAnoPub(int anoPublicacao) {
+    	this.anoPublicacao = anoPublicacao; 
+    }
  
     void exibirDetalhes() {
         System.out.println("Título: " + this.titulo);
